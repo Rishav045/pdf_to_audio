8ce2781f4f1162ab5a46beebba70dd47268927b8
Rishav045 <82647816+Rishav045@users.noreply.github.com>
2024-03-01 16:48:35 +0000

    Dowloading embedding_and_chunk

--- main.py
+++ main.py
@@ -118,6 +118,137 @@ def summary_to_audio(pdf_url):
   delete_folder(pdf_url.split('.')[0])
   return links
 
+def chunking_for_embedding(pdf_url):
+  delete_folder(pdf_url.split('.')[0])
+  create_folder(pdf_url.split('.')[0])
+
+
+  summary =  pdf_scrapper_summary(f"uploads/{pdf_url}")
+  summaryText=""
+  for item in summary:
+    summaryText= summaryText+" "+item
+  print(f'Length of the summary = {len(summary[0].split())}')
+  summary_Text_List= summaryText.split()
+  print(f'summary_Text_List length = {len(summary_Text_List)}')
+  chunks=[]
+  text=""
+  if(len(summary_Text_List)<500):
+     for i in range(0,len(summary_Text_List)):
+      text = text+" "+summary_Text_List[i]
+     chunks.append(text)
+     return chunks
+     
+  for i in range(0,500):
+     text = text+" "+summary_Text_List[i]
+  chunks.append(text)
+  start =500
+  while(start<len(summary_Text_List)):
+    text=""
+    start= start-50
+    if(start+500>=len(summary_Text_List)):
+      for i in range(start,len(summary_Text_List)):
+         text= text+" "+summary_Text_List[i]
+      chunks.append(text)
+      break
+    for i in range(start,start+500):
+      text = text +" "+summary_Text_List[i]
+    chunks.append(text)
+    start= start +500
+    text =""
+    if start>=len(summary_Text_List):
+       for i in range(start-500,len(summary_Text_List)):
+          text = text+" "+summary_Text_List[i]
+       chunks.append(text)
+       break
+  print(len(chunks))
+  return chunks
+
+def extract_embedding1(chunks):
+  #  model1=genai.embed_content()
+  embeddings =[]
+  for item in chunks:
+      result = genai.embed_content(
+      model="models/embedding-001",
+      content=item,
+      task_type="retrieval_document")
+      embeddings.append(result['embedding'])
+  return embeddings
+
+ 
+def save_extract_embedding(pdf_url):
+  find_url= check_existing_embedding('embeddings',f"{pdf_url.split('.')[0]}")
+  print(f"{pdf_url.split('.')[0]}.pickle")
+  if(len(find_url)==0): 
+    chunks = chunking_for_embedding(pdf_url)
+    create_folder('chunks')
+    with open("chunks/chunks.pkl","wb") as file:
+       pickle.dump(chunks,file)
+    result = cloudinary.uploader.upload("chunks/chunks.pkl", resource_type="auto" ,format="pickle" , public_id = f"chunks/{pdf_url.split('.')[0]}")
+    print(result['url'])
+    delete_folder('chunks')
+  embeddings=[]
+  print(find_url)
+  if(len(find_url)):
+    print("embeddings  found in the cludinary database ")
+    delete_folder('embeddings')
+    create_folder('embeddings')
+    size = len(find_url)
+    for item in find_url:
+      response = requests.get(item['secure_url'],stream=True)
+    
+      with open (item['public_id'],'wb') as file:
+        for chunk in response.iter_content(chunk_size=1024):
+          if chunk:
+             file.write(chunk)
+      with open (item['public_id'],"rb") as file:
+        embedding=pickle.load(file)
+      for it in embedding:
+         embeddings.append(it)
+    print(f"Length of total embedding {len(embeddings)}")
+    
+  else:
+     print("Calling for creating the embedding")
+     embeddings = extract_embedding1(chunks)
+     delete_folder('embeddings')
+     create_folder('embeddings')
+     with open ('embeddings/embeddings.pkl','wb') as file:
+        pickle.dump(embeddings,file)
+     result = cloudinary.uploader.upload("embeddings/embeddings.pkl", resource_type="auto" ,format="pickle" , public_id = f"embeddings/{pdf_url.split('.')[0]}")
+     print(result['url'])
+     delete_folder('embeddings')
+  return embeddings
+
+def check_existing_embedding(folder_name,file_name):
+  print(f"{folder_name} {file_name}")
+  search_result = cloudinary.Search()\
+    .expression(f'{folder_name}/{file_name}*')\
+    .sort_by("filename", "asc")\
+    .execute()
+  print(len(search_result['resources']))
+  return search_result['resources']
+
+
+
+def chunk_and_embedding(pdf_url):
+  embeddings = save_extract_embedding(pdf_url)
+  chunks =[]
+  find_url=check_existing_embedding('chunks',f"{pdf_url.split('.')[0]}")
+  print(find_url)
+  create_folder('chunks')
+  for item in find_url:
+    response = requests.get(item['secure_url'],stream=True)
+  
+    with open (item['public_id'],'wb') as file:
+      for chunk in response.iter_content(chunk_size=1024):
+        if chunk:
+          file.write(chunk)
+    with open (item["public_id"],"rb") as file:
+       chunk=pickle.load(file)
+    for it in chunk:
+       chunks.append(it)
+  print(f"Length of the chunks = {len(chunks)} and Length of the embedding = {len(embeddings)}")
+  return {"chunks":chunks , "embeddings":embeddings}
+
 app = FastAPI()
 
 app.add_middleware(
@@ -176,5 +307,20 @@ async def getAudio(file : UploadFile= File(...)):
   
      
   # return {"filename":file.filename }
+@app.post("/embedding_chunk")
+async def get_embedding_chunk(file:UploadFile= File(...)):
+    upload_dir = "uploads"
 
+    # Create the directory if it doesn't exist
+    os.makedirs(upload_dir, exist_ok=True)
+
+    # Define the file path where you want to save the uploaded file
+    file_path = os.path.join(upload_dir, file.filename)
+
+    # Open the file and write the contents
+    with open(file_path, "wb") as f:
+        shutil.copyfileobj(file.file, f)
+    result = chunk_and_embedding(file.filename)
+    delete_folder("uploads")
+    return result
 
